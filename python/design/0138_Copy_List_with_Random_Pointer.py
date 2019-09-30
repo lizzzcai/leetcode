734788af21827644aea7d5f39365a3d04a1e4317
Lizzzcai <lilcolinn@gmail.com>
2019-10-01 00:53:40 +0800

    update 0023 piority queue

---
+++ python/design/0138_Copy_List_with_Random_Pointer.py
@@ -0,0 +1,77 @@
+"""
+22/09/2019
+138. Copy List with Random Pointer - Medium
+Tag: dict, linked list
+
+
+A linked list is given such that each node contains an additional random pointer which could point to any node in the list or null.
+
+Return a deep copy of the list.
+
+ 
+
+Example 1:
+
+
+
+Input:
+{"$id":"1","next":{"$id":"2","next":null,"random":{"$ref":"2"},"val":2},"random":{"$ref":"2"},"val":1}
+
+Explanation:
+Node 1's value is 1, both of its next and random pointer points to Node 2.
+Node 2's value is 2, its next pointer points to null and its random pointer points to itself.
+ 
+
+Note:
+
+You must return the copy of the given head as a reference to the cloned list.
+
+"""
+
+
+
+
+# Definition for a Node.
+class Node:
+    def __init__(self, val, next, random):
+        self.val = val
+        self.next = next
+        self.random = random
+
+class Solution:
+    def copyRandomList(self, head: 'Node') -> 'Node':
+        # dict to store each Node of head,
+        # key is the node in the head, value is the new node.
+        hmap = {}
+        # have two copys of head
+        m, n = head, head
+        while m:
+            # for each node in head, make a copy with value
+            hmap[m] = Node(m.val, None, None)
+            m = m.next
+        
+        while n:
+            # for each node in head, get the copy node from dict and update the next and random value
+            hmap[n].next = hmap.get(n.next)
+            hmap[n].random = hmap.get(n.random)
+            n = n.next
+        
+        # return the head node of the copy linked list
+        return hmap.get(head)
+
+
+class Solution1:
+    def copyRandomList(self, head: 'Node') -> 'Node':
+        # dict to store each Node of head,
+        # key is the node in the head, value is the new node.
+        hmap = {}
+        hmap[None] = None
+        # have a of head
+        n = head
+        
+        while n:
+            hmap.setdefault(n, Node(None,None,None)).val = n.val
+            hmap[n].next = hmap.setdefault(n.next, Node(None,None,None))
+            hmap[n].random = hmap.setdefault(n.random, Node(None,None,None))
+            n = n.next
+        return hmap[head]
--- python/linked_list/0023_Merge_k_Sorted_Lists.py
+++ python/linked_list/0023_Merge_k_Sorted_Lists.py
@@ -39,6 +39,48 @@ class Solution:
         
         return head.next
 
+
+from queue import PriorityQueue
+from dataclasses import dataclass, field
+from typing import Any
+
+@dataclass(order=True)
+class PrioritizedItem_dc:
+    priority: int
+    item: Any=field(compare=False)
+
+class PrioritizedItem:
+    def __init__(self, priority, item):
+        self.priority = priority
+        self.item = item
+
+    def __lt__(self, other):
+        return self.priority <  other.priority
+
+class Solution1:
+    def mergeKLists(self, lists: List[ListNode]) -> ListNode:
+        '''
+        piorityQ
+        Time: O(Nlogk)
+        Space O(1)
+        '''
+        q = PriorityQueue()
+        for l in lists:
+            if l:
+                q.put(PrioritizedItem(l.val, l))
+        
+        head = point = ListNode(None)
+        while not q.empty():
+            item = q.get()
+            val, node = item.priority, item.item
+            point.next = ListNode(val)
+            point = point.next
+            node = node.next
+            if node:
+                q.put(PrioritizedItem(node.val, node))
+            
+        return head.next
+
 def list_to_linkedlist(val_list):
     head = node = ListNode(None)
     for val in val_list:
@@ -53,6 +95,7 @@ def linkedlist_to_list(head):
         head = head.next
     return res
 
+
 # Unit Test
 import unittest
 class mergeKListsCase(unittest.TestCase):
@@ -63,7 +106,7 @@ class mergeKListsCase(unittest.TestCase):
         pass
 
     def test_mergeKLists(self):
-        func = Solution().mergeKLists
+        func = Solution1().mergeKLists
         l1 = list_to_linkedlist([1,4,5])
         l2 = list_to_linkedlist([1,3,4])
         l3 = list_to_linkedlist([2,6])
