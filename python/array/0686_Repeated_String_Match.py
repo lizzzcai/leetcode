efa9db66dc0832735039978955c69bce1681b19f
Lizzzcai <lilcolinn@gmail.com>
2019-12-28 21:18:26 +0800

    update 0686 repeated string match, add comment on solution

--- python/array/0686_Repeated_String_Match.py
+++ python/array/0686_Repeated_String_Match.py
@@ -19,12 +19,25 @@ import math
 
 # Solution
 class Solution:
-    '''
-    Time complexity : O(N*(N+M))
-    Space complexity : O(N+M)
-    '''
     def repeatedStringMatch(self, A: str, B: str) -> int:
+        
+        '''
+        Time: O(N*(N+M)) where M N is the length of A, B.
+        # len of S at most O(M+N). When checking whether B is a substring of A,
+        this check takes naively the product of their lengths.
+        
+        SpacdL O(M+N)
+        
+        Now, suppose q is the least number for which len(B) <= len(A * q). 
+        We only need to check whether B is a substring of A * q or A * (q+1).
+        If we try k < q, then B has larger length than A * q and therefore can't be a substring.
+        When k = q+1, A * k is already big enough to try all positions for B; namely, A[i:i+len(B)] == B for i = 0, 1, ..., len(A) - 1.
+
+        '''
+        # the least number for which len(B) <= len(A * q)
         q = math.ceil(len(B)/len(A))
+        # As S is the repeat of A, which has period at most len(A)
+        # So q + 1 is big enough to try all positions for B.
         for i in range(2):
             if B in A * (q+i):
                 return q+i
@@ -42,6 +55,7 @@ class repeatedStringMatchCase(unittest.TestCase):
     def test_repeatedStringMatchCase(self):
         func = Solution().repeatedStringMatch
         self.assertEqual(func("abcd", "cdabcdab"), 3)
+        self.assertEqual(func("abce", "cdabcdab"), -1)
 
 if __name__ == '__main__':
     unittest.main()
