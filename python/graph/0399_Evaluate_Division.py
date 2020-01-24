3cfa72626e9e40d10feb96aa5cb73325655e5764
Lizzzcai <lilcolinn@gmail.com>
2020-01-24 23:50:30 +0800

    update 399 solutions

--- python/graph/0399_Evaluate_Division.py
+++ python/graph/0399_Evaluate_Division.py
@@ -27,6 +27,8 @@ The input is always valid. You may assume that evaluating the queries will resul
 '''
 
 from typing import List
+from collections import deque, defaultdict
+
 # Solution
 class Solution1:
     '''
@@ -34,83 +36,88 @@ class Solution1:
     Space complexity : O(n)
     '''
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
-        graph = dict()
-        for idx, equation in enumerate(equations):
-            numerator, denominator = equation[0], equation[1]
-            # add into graph
-            # a / a = 1, b / b = 1
-            graph.setdefault(numerator, {numerator : 1.0})
-            graph.setdefault(denominator, {denominator : 1.0})
-            # a / b, b / a
-            value = values[idx]
-            graph[numerator][denominator] = value
-            graph[denominator][numerator] = 1. / value
-            
+
+        graph = self.build_graph(equations, values)
         res = []
         for query in queries:
             visited = set()
-            val = self.dfs(graph, visited, query[0], query[1])
-            if val:
-                res.append(val)
-            else:
-                res.append(-1.0)
-        
+            val = self.dfs(graph, visited, query[0], query[1], 1.0)
+            res.append(val)
         return res
     
-    def dfs(self, graph, visited, src, dst):
+    def build_graph(self, equations, values):
+        graph = defaultdict(dict)
+
+        def add_edge(numerator, denominator, value):
+            graph[numerator][denominator] = value
+        
+        for ([numerator, denominator], value) in zip(equations, values):
+            add_edge(numerator, denominator, value)
+            add_edge(denominator, numerator, 1./value)
+        
+        return graph
+
+    def dfs(self, graph, visited, src, dst, value):
+        if src not in graph or dst not in graph:
+            return -1.0
+        
+        if src == dst:
+            return value
+    
         visited.add(src)
         src_node = graph.get(src)
-        if src_node:
-            val = src_node.get(dst)
-            if val:
-                return val
+        for connection in src_node:
+            if connection not in visited:
+                val = self.dfs(graph, visited, connection, dst, src_node[connection] * value)
+                if val != -1.0:
+                    return val
 
-            for connection in src_node:
-                if connection not in visited:
-                    val = self.dfs(graph, visited, connection, dst)
-                    if val:
-                        return src_node[connection] * val
-        return None
+        return -1.0
 
-from collections import deque
 class Solution2:
     '''
     Time complexity : O(n)
     Space complexity : O(n)
     '''
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
-        graph = dict()
-        for idx, equation in enumerate(equations):
-            numerator, denominator = equation[0], equation[1]
-            # add into graph
-            # a / a = 1, b / b = 1
-            graph.setdefault(numerator, {numerator : 1.0})
-            graph.setdefault(denominator, {denominator : 1.0})
-            value = values[idx]
-            # a / b, b / a
-            graph[numerator][denominator] = value
-            graph[denominator][numerator] = 1. / value
-            
+        graph = self.build_graph(equations, values)
         res = []
         for query in queries:
             val = self.bfs(graph, query[0], query[1])
             res.append(val)
+            if val != -1.0:
+                # add cache
+                graph[query[0]][query[1]] = val
+                graph[query[1]][query[0]] = 1./val
         return res
+    
+    def build_graph(self, equations, values):
+        graph = defaultdict(dict)
+
+        def add_edge(numerator, denominator, value):
+            graph[numerator][denominator] = value
+        
+        for ([numerator, denominator], value) in zip(equations, values):
+            add_edge(numerator, denominator, value)
+            add_edge(denominator, numerator, 1./value)
+        
+        return graph
     
     def bfs(self, graph, src, dst):
         if src not in graph or dst not in graph:
             return -1.0
+
         q = deque([(src, 1.0)])
         visited = set()
 
         while q:
-            front, curr_product = q.popleft()
-            if front == dst:
+            node, curr_product = q.popleft()
+            if node == dst:
                 return curr_product
-            visited.add(front)
-            for neighbor, val in graph[front].items():
+            for neighbor, val in graph[node].items():
                 if neighbor not in visited:
                     q.append((neighbor, curr_product * val))
+                    visited.add(neighbor)
         
         return -1.0
 
