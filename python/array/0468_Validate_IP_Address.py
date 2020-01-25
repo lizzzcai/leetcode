29cde78c8d69607134921d415d1a107d027e43ea
Lizzzcai <lilcolinn@gmail.com>
2020-01-26 00:13:10 +0800

    add 0468 validate ip address

---
+++ python/array/0468_Validate_IP_Address.py
@@ -0,0 +1,123 @@
+'''
+26/01/2020
+
+468. Validate IP Address - Medium
+
+Tag: String
+
+Write a function to check whether an input string is a valid IPv4 address or IPv6 address or neither.
+
+IPv4 addresses are canonically represented in dot-decimal notation, which consists of four decimal numbers, each ranging from 0 to 255, separated by dots ("."), e.g.,172.16.254.1;
+
+Besides, leading zeros in the IPv4 is invalid. For example, the address 172.16.254.01 is invalid.
+
+IPv6 addresses are represented as eight groups of four hexadecimal digits, each group representing 16 bits. The groups are separated by colons (":"). For example, the address 2001:0db8:85a3:0000:0000:8a2e:0370:7334 is a valid one. Also, we could omit some leading zeros among four hexadecimal digits and some low-case characters in the address to upper-case ones, so 2001:db8:85a3:0:0:8A2E:0370:7334 is also a valid IPv6 address(Omit leading zeros and using upper cases).
+
+However, we don't replace a consecutive group of zero value with a single empty group using two consecutive colons (::) to pursue simplicity. For example, 2001:0db8:85a3::8A2E:0370:7334 is an invalid IPv6 address.
+
+Besides, extra leading zeros in the IPv6 is also invalid. For example, the address 02001:0db8:85a3:0000:0000:8a2e:0370:7334 is invalid.
+
+Note: You may assume there is no extra space or special characters in the input string.
+
+Example 1:
+Input: "172.16.254.1"
+
+Output: "IPv4"
+
+Explanation: This is a valid IPv4 address, return "IPv4".
+Example 2:
+Input: "2001:0db8:85a3:0:0:8A2E:0370:7334"
+
+Output: "IPv6"
+
+Explanation: This is a valid IPv6 address, return "IPv6".
+Example 3:
+Input: "256.256.256.256"
+
+Output: "Neither"
+
+Explanation: This is neither a IPv4 address nor a IPv6 address.
+
+'''
+
+from typing import List
+# Solution
+class Solution:
+    '''
+    Time complexity : O(n)
+    Space complexity : O(n)
+    '''
+    def validIPAddress(self, IP: str) -> str:
+        if not IP:
+            return "Neither"
+        if '.' in IP[:5] and self.is_ipv4(IP):
+            return "IPv4"
+        elif ':' in IP[:5] and self.is_ipv6(IP):
+            return "IPv6"
+        else:
+            return "Neither"
+            
+    
+    def is_ipv4(self, IP):
+        strs = IP.split('.')
+        if len(strs) != 4:
+            return False
+        
+        for s in strs:
+            if not s or len(s) >3 or not s.isdigit():
+                return False
+            num = int(s)
+            if str(num) != s or num < 0 or num > 255:
+                return False
+            
+        return True
+    
+    def is_ipv6(self, IP):
+        
+        def is_hex(s):
+            hex_digits = set("0123456789abcdefABCDEF")
+            for char in s:
+                if not (char in hex_digits):
+                    return False
+            return True
+                
+        strs = IP.split(':')
+        if len(strs) != 8:
+            return False
+        for s in strs:
+            if not s or len(s) > 4 or not s.isalnum() or not is_hex(s):
+                return False
+            num = int(s, 16)
+            if num < 0:
+                return False
+            
+        return True
+
+"20EE:FGb8:85a3:0:0:8A2E:0370:7334"
+"2001:0db8:85a3:0:0:8A2E:0370:7334"
+"192.0.0.1"
+
+# Unit Test
+import unittest
+class TestCase(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_testCase(self):
+        func = Solution().validIPAddress
+        self.assertEqual(func("172.16.254.1"), "IPv4")
+        self.assertEqual(func("2001:0db8:85a3:0:0:8A2E:0370:7334"), "IPv6")
+        self.assertEqual(func("256.256.256.256"), "Neither")
+        self.assertEqual(func("172.16.254.01"), "Neither")
+
+
+        self.assertEqual(func("20EE:FGb8:85a3:0:0:8A2E:0370:7334"), "Neither")
+        self.assertEqual(func("2001:0db8:85a3:0:0:8A2E:0370:7334"), "IPv6")
+        self.assertEqual(func("192.0.0.1"), "IPv4")
+
+
+if __name__ == '__main__':
+    unittest.main()
