fb2574a648269d210eafa0842b17eb5f60a8e8ff
Lizzzcai <lilcolinn@gmail.com>
2020-02-08 02:49:39 +0800

    reservoir sampling problems 0382, 0398

---
+++ python/math/0382_Linked_List_Random_Node.py
@@ -0,0 +1,91 @@
+'''
+08/02/2020
+
+382. Linked List Random Node - Medium
+
+Tag: Reservoir Sampling
+
+Given a singly linked list, return a random node's value from the linked list. Each node must have the same probability of being chosen.
+
+Follow up:
+What if the linked list is extremely large and its length is unknown to you? Could you solve this efficiently without using extra space?
+
+Example:
+
+// Init a singly linked list [1,2,3].
+ListNode head = new ListNode(1);
+head.next = new ListNode(2);
+head.next.next = new ListNode(3);
+Solution solution = new Solution(head);
+
+// getRandom() should return either 1, 2, or 3 randomly. Each element should have equal probability of returning.
+solution.getRandom();
+
+
+'''
+
+from typing import List
+# Solution
+from random import randrange
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+class Solution:
+
+    def __init__(self, head: ListNode):
+        """
+        @param head The linked list's head.
+        Note that the head is guaranteed to be not null, so it contains at least one node.
+        """
+        self.head = head
+
+    def getRandom(self) -> int:
+        """
+        Returns a random node's value.
+        """
+        '''
+        https://leetcode.com/problems/linked-list-random-node/discuss/85659/Brief-explanation-for-Reservoir-Sampling
+          1/i * (1 - 1/(i+1)) * (1 - 1/(i+2)) * ... * (1 - 1/n)
+        = 1/i * i/(i+1) * (i+1)/(i+2) * ... * (n-1)/n
+        = 1/n
+        
+        '''
+        p = self.head
+        i, res = 0, None
+        while p:
+            i += 1
+            if randrange(1, i+1) == i:
+                res = p.val
+            p = p.next
+        
+        return res
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(head)
+# param_1 = obj.getRandom()
+
+# Unit Test
+import unittest
+class TestCase(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_testCase(self):
+        
+        head = ListNode(1)
+        head.next = ListNode(2)
+        head.next.next = ListNode(3)
+
+        # Your Solution object will be instantiated and called as such:
+        obj = Solution(head)
+        param_1 = obj.getRandom()
+
+        self.assertIn(param_1, [0,1,2])
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ python/math/0398_Random_Pick_Index.py
@@ -0,0 +1,70 @@
+'''
+08/02/2020
+
+398. Random Pick Index - Medium
+
+Tag: Reservoir Sampling
+
+Given an array of integers with possible duplicates, randomly output the index of a given target number. You can assume that the given target number must exist in the array.
+
+Note:
+The array size can be very large. Solution that uses too much extra space will not pass the judge.
+
+Example:
+
+int[] nums = new int[] {1,2,3,3,3};
+Solution solution = new Solution(nums);
+
+// pick(3) should return either index 2, 3, or 4 randomly. Each index should have equal probability of returning.
+solution.pick(3);
+
+// pick(1) should return 0. Since in the array only nums[0] is equal to 1.
+solution.pick(1);
+
+'''
+
+from typing import List
+# Solution
+from random import randrange
+class Solution:
+
+    def __init__(self, nums: List[int]):
+        self.arr = nums
+        
+
+    def pick(self, target: int) -> int:
+        i = 0
+        res = None
+        for idx, num in enumerate(self.arr):
+            if num != target:
+                continue
+            i += 1
+            if randrange(1, i+1) == i:
+                res = idx
+        
+        return res
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(nums)
+# param_1 = obj.pick(target)
+
+# Unit Test
+import unittest
+class TestCase(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_testCase(self):
+        nums = [1,2,3,3,3]
+        target = 3
+        obj = Solution(nums)
+        param_1 = obj.pick(target)
+
+        #self.assertEqual(param_1, [0, 1])
+        self.assertIn(param_1, [2, 3, 4])
+
+if __name__ == '__main__':
+    unittest.main()
