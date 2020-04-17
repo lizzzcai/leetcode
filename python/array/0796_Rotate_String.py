f195ee254cbdd64eeaf20eb36aa1ee990f62a0c5
lizzzcai <lilcolinn@gmail.com>
2020-04-17 16:56:25 +0800

    add solution to 0796 rolling hash and kmp

--- python/array/0796_Rotate_String.py
+++ python/array/0796_Rotate_String.py
@@ -54,6 +54,86 @@ class Solution2:
         '''
         return len(A) == len(B) and B in A+A
 
+
+class Solution3:
+    def rotateString(self, A: str, B: str) -> bool:
+        '''
+        Rolling hash
+        
+        
+        O(n)
+        O(n)
+        '''
+        
+        MOD = 10 ** 9 + 7
+        P = 113
+        Pinv = pow(P, MOD-2, MOD) # pow(x, y, z) is equal to x^y % z
+        
+        hb = 0
+        power = 1
+        for x in B:
+            code = ord(x) - ord('a') + 1
+            hb = (hb + power*code) % MOD
+            power = power * P % MOD
+        
+        ha = 0
+        power = 1
+        for x in A:
+            code = ord(x) - ord('a') + 1
+            ha = (ha + power*code) % MOD
+            power = power * P % MOD
+        
+        if ha == hb and A == B:
+            return True
+        
+        for i, x in enumerate(A):
+            code = ord(x) - ord('a') + 1
+            ha += power * code
+            ha -= code
+            ha *= Pinv
+            ha %= MOD
+            if ha == hb and A[i+1:] + A[:i+1] == B:
+                return True
+            
+
+class Solution4:
+    def rotateString(self, A: str, B: str) -> bool:
+        '''
+        KMP(Knuth-Morris-Pratt) 
+        
+        https://leetcode.com/articles/rotate-string/
+        
+        O(N) where N is the length of A
+        O(N) to create shift table shifts
+        '''
+
+        N = len(A)
+        if N != len(B):
+            return False
+        if N == 0:
+            return True
+        
+        # Compute shift table
+        shifts = [1] * (N+1)
+        left = -1
+        for right in range(N):
+            while left >= 0 and B[left] != B[right]:
+                left -= shifts[left]
+            shifts[right+1] = right - left
+            left += 1
+        
+        # Find match of B in A+A
+        match_len = 0
+        for char in A+A:
+            while match_len >= 0 and B[match_len] != char:
+                match_len -= shifts[match_len]
+                
+            match_len += 1
+            if match_len == N:
+                return True
+        
+        return False
+
 # Unit Test
 import unittest
 class TestCase(unittest.TestCase):
@@ -64,7 +144,7 @@ class TestCase(unittest.TestCase):
         pass
 
     def test_testCase(self):
-        for Sol in [Solution1(), Solution2()]:
+        for Sol in [Solution1(), Solution2(), Solution3(), Solution4()]:
             func = Sol.rotateString
             self.assertEqual(func('abcde', 'cdeab'), True)
             self.assertEqual(func('abcde', 'abced'), False)
