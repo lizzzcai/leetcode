1f5aa9d11556d17dbfc9be8f5cdf13e4527e0334
lizzzcai <lilcolinn@gmail.com>
2020-07-10 08:59:55 +0800

    add 0062

---
+++ python/tree/0662_Maximum_Width_of_Binary_Tree.py
@@ -0,0 +1,106 @@
+'''
+01/06/2020
+
+662. Maximum Width of Binary Tree - Medium
+
+Tag: Tree
+
+Given a binary tree, write a function to get the maximum width of the given tree. The width of a tree is the maximum width among all levels. The binary tree has the same structure as a full binary tree, but some nodes are null.
+
+The width of one level is defined as the length between the end-nodes (the leftmost and right most non-null nodes in the level, where the null nodes between the end-nodes are also counted into the length calculation.
+
+Example 1:
+
+Input: 
+
+           1
+         /   \
+        3     2
+       / \     \  
+      5   3     9 
+
+Output: 4
+Explanation: The maximum width existing in the third level with the length 4 (5,3,null,9).
+Example 2:
+
+Input: 
+
+          1
+         /  
+        3    
+       / \       
+      5   3     
+
+Output: 2
+Explanation: The maximum width existing in the third level with the length 2 (5,3).
+Example 3:
+
+Input: 
+
+          1
+         / \
+        3   2 
+       /        
+      5      
+
+Output: 2
+Explanation: The maximum width existing in the second level with the length 2 (3,2).
+Example 4:
+
+Input: 
+
+          1
+         / \
+        3   2
+       /     \  
+      5       9 
+     /         \
+    6           7
+Output: 8
+Explanation:The maximum width existing in the fourth level with the length 8 (6,null,null,null,null,null,null,7).
+
+
+Note: Answer will in the range of 32-bit signed integer.
+
+'''
+
+from typing import List
+import collections
+from tree_utils import TreeNode, deserialize, serialize
+# Solution
+class Solution1:
+    def widthOfBinaryTree(self, root: TreeNode) -> int:
+        ans = 0
+        if not root:
+            return ans
+        
+        queue = collections.deque([(root, 0)])
+        while queue:
+            l, r = queue[0][1], queue[-1][1]
+            ans = max(ans, r-l + 1)
+            
+            for _ in range(len(queue)):
+                node, idx = queue.popleft()
+                if node.left:
+                    queue.append((node.left, idx*2+1))
+                if node.right:
+                    queue.append((node.right, idx*2+2))
+                
+        return ans
+
+# Unit Test
+import unittest
+class TestCase(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_testCase(self):
+        for Sol in [Solution1()]:
+            func = Sol.widthOfBinaryTree
+            self.assertEqual(func(deserialize("[1,3,2,5,3,null,9]")), 4)
+
+if __name__ == '__main__':
+    unittest.main()
