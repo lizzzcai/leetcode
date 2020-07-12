792088913fcc86e4c74d586d57cff4246e46a882
lizzzcai <lilcolinn@gmail.com>
2020-07-13 06:31:20 +0800

    add 0190 reverse bits

---
+++ python/bit_manipulation/0190_Reverse_Bits.py
@@ -0,0 +1,66 @@
+'''
+13/07/2020
+
+190. Reverse Bits - Easy
+
+Tag: Bit Manipulation
+
+Reverse bits of a given 32 bits unsigned integer.
+
+ 
+
+Example 1:
+
+Input: 00000010100101000001111010011100
+Output: 00111001011110000010100101000000
+Explanation: The input binary string 00000010100101000001111010011100 represents the unsigned integer 43261596, so return 964176192 which its binary representation is 00111001011110000010100101000000.
+Example 2:
+
+Input: 11111111111111111111111111111101
+Output: 10111111111111111111111111111111
+Explanation: The input binary string 11111111111111111111111111111101 represents the unsigned integer 4294967293, so return 3221225471 which its binary representation is 10111111111111111111111111111111.
+ 
+
+Note:
+
+Note that in some languages such as Java, there is no unsigned integer type. In this case, both input and output will be given as signed integer type and should not affect your implementation, as the internal binary representation of the integer is the same whether it is signed or unsigned.
+In Java, the compiler represents the signed integers using 2's complement notation. Therefore, in Example 2 above the input represents the signed integer -3 and the output represents the signed integer -1073741825.
+ 
+
+Follow up:
+
+If this function is called many times, how would you optimize it?
+
+'''
+
+from typing import List
+# Solution
+class Solution1:
+    '''
+    Time complexity : O(1)
+    Space complexity : O(1)
+    '''
+    def reverseBits(self, n: int) -> int:
+        res = 0
+        for i in range(32):
+            res = (res << 1) + (n & 1)
+            n >>= 1
+        
+        return res
+
+# Unit Test
+import unittest
+class TestCase(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_testCase(self):
+        for Sol in [Solution1()]:
+            func = Sol.reverseBits
+            self.assertEqual(func(int("00000010100101000001111010011100", 2)), 964176192)
+
+if __name__ == '__main__':
+    unittest.main()
