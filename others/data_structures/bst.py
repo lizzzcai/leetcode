551cd3b4616c16a6562eb7c577ce671b419f0616
lizzzcai <lizzzcai1@gmail.com>
2020-12-07 11:07:58 +0800

    init BST in data structures

---
+++ others/data_structures/bst.py
@@ -0,0 +1,31 @@
+class TreeNode:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+    def __repr__(self):
+        return 'TreeNode({})'.format(self.val)
+    
+def deserialize(string):
+    if string == '{}':
+        return None
+    nodes = [None if val == 'null' else TreeNode(int(val))
+             for val in string.strip('[]{}').split(',')]
+    kids = nodes[::-1]
+    root = kids.pop()
+    for node in nodes:
+        if node:
+            if kids: node.left  = kids.pop()
+            if kids: node.right = kids.pop()
+    return root
+
+def serialize(root):
+    def doit(node):
+        if node:
+            vals.append(str(node.val))
+            doit(node.left)
+            doit(node.right)
+
+    vals = []
+    doit(root)
+    return '[' + ','.join(vals) + ']'
